ccc8537a3bacd143e9bb30a0154b5c6160b9dcaf
vlad.egorov <vladxd228@gmail.com>
2019-06-04 10:33:56 +0300

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+3.times do
+  role = Role.create(name: Faker::Name.unique.name)
+  NotificationTemplate.create(name: Faker::Name.unique.name, role_id: role.id )
+end
+
+3.times do
+  Company.create(name: Faker::Company.name, role_id: Role.first.id)
+end
